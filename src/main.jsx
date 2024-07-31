5b0bff950c21573d664d088dfa930f5da42729a6
Vardaan Bhatia <vardaanbhatia55@gmail.com>
2024-08-01 01:48:42 +0530

    netlify file issue fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import WatchProvider from "./Context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
